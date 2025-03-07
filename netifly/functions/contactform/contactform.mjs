8fed0349437f59caa3af5f7423bb4f422505c966
Elkadi, Omar (EXTERN: PowerCo DE OG-D3) <omar.elkadi@cybus.io>
2025-03-07 13:11:05 +0100

    update destination email in contact form serverless function

--- netifly/functions/contactform/contactform.mjs
+++ netifly/functions/contactform/contactform.mjs
@@ -66,7 +66,7 @@ ${message}`;
 
   const mailOptions = {
     from: process.env.GMAIL_USER,
-    to: 'elkadi.omar.oe2@example.com', // Change to your destination email
+    to: 'elkadi.omar.oe2@gmail.com', // Change to your destination email
     subject: `${inquiry} from ${name}`,
     text,
   };
